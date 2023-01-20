9c409e9de73e3e3ec6a00c81269aeda72784ea96
MrKono <fumisan1126@icloud.com>
2023-01-20 19:18:37 +0900

    GTCEu 2.5.0

--- src/main/java/kono/materialreplication/recipe/MRRecipeMaps.java
+++ src/main/java/kono/materialreplication/recipe/MRRecipeMaps.java
@@ -4,7 +4,7 @@ import gregtech.api.gui.GuiTextures;
 import gregtech.api.gui.widgets.ProgressWidget;
 import gregtech.api.recipes.RecipeMap;
 import gregtech.api.recipes.builders.SimpleRecipeBuilder;
-import gregtech.api.sound.GTSounds;
+import gregtech.core.sound.GTSoundEvents;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenProperty;
 
@@ -17,7 +17,7 @@ public class MRRecipeMaps {
             0, 1, // FluidInput (min/max)
             2, 2, // FluidOutput (min/max)
             new SimpleRecipeBuilder(), false)
-            .setSound(GTSounds.ELECTROLYZER)
+            .setSound(GTSoundEvents.SCIENCE)
             .setSlotOverlay(false, false, GuiTextures.LIGHTNING_OVERLAY_1) // Item Input
             .setSlotOverlay(false, true, GuiTextures.LIGHTNING_OVERLAY_2) // Fluid Input
             .setSlotOverlay(true, true, false, GuiTextures.MOLECULAR_OVERLAY_3) // Fluid Output1
@@ -31,11 +31,11 @@ public class MRRecipeMaps {
             0, 3,
             0, 1,
             new SimpleRecipeBuilder(), false)
-            .setSound(GTSounds.ELECTROLYZER)
+            .setSound(GTSoundEvents.REPLICATOR)
             .setSlotOverlay(false, false, GuiTextures.DUST_OVERLAY) // Item Input
             .setSlotOverlay(false, true, false, GuiTextures.MOLECULAR_OVERLAY_3) // Fluid Input 1 & 2
             .setSlotOverlay(false, true, true, GuiTextures.VIAL_OVERLAY_2) // Fluid Input 3
             .setSlotOverlay(true, false, GuiTextures.MOLECULAR_OVERLAY_1) // Item Output
             .setSlotOverlay(true, true, GuiTextures.VIAL_OVERLAY_2) // Fluid Output
-            .setProgressBar(GuiTextures.PROGRESS_BAR_CIRCUIT, ProgressWidget.MoveType.HORIZONTAL);
+            .setProgressBar(GuiTextures.PROGRESS_BAR_REPLICATOR, ProgressWidget.MoveType.HORIZONTAL);
 }
