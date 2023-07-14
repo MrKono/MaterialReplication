cc07daf24b4969bde036da293161ed3349af6a9b
ko_no <90126004+MrKono@users.noreply.github.com>
2023-07-14 15:17:55 +0900

    1.1.0 part.4

--- src/main/java/kono/ceu/materialreplication/common/MREventHandlers.java
+++ src/main/java/kono/ceu/materialreplication/common/MREventHandlers.java
@@ -2,8 +2,10 @@ package kono.ceu.materialreplication.common;
 
 
 import gregtech.api.unification.material.event.MaterialEvent;
+import gregtech.api.unification.material.event.PostMaterialEvent;
 import kono.ceu.materialreplication.api.MRValues;
 import kono.ceu.materialreplication.api.unification.materials.MRMaterials;
+import kono.ceu.materialreplication.api.unification.materials.flags.MRMaterialFlagAddition;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -17,9 +19,9 @@ public class MREventHandlers {
         MRMaterials.orePrefix();
     }
 
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public static void registerMaterialsPost(MaterialEvent event) {
-        //MRMaterialFlagAddition.intLate();
+    @SubscribeEvent
+    public static void registerMaterialsPost(PostMaterialEvent event) {
+        MRMaterialFlagAddition.intLate();
     }
 
 }
