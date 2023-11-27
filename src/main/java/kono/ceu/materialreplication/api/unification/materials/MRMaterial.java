61e51e48bcbbc878e9fd81e0fce67c69d890ed97
ko_no <90126004+MrKono@users.noreply.github.com>
2023-11-27 21:05:50 +0900

    Update GTCEu 2.8.0-beta

--- src/main/java/kono/ceu/materialreplication/api/unification/materials/MRMaterial.java
+++ src/main/java/kono/ceu/materialreplication/api/unification/materials/MRMaterial.java
@@ -1,5 +1,6 @@
 package kono.ceu.materialreplication.api.unification.materials;
 
+import gregtech.api.fluids.FluidBuilder;
 import gregtech.api.unification.material.Material;
 import gregtech.api.unification.material.info.MaterialIconSet;
 
@@ -10,26 +11,24 @@ public class MRMaterial {
     public static void init() {
 
         MRMaterials.PrimalMatter = new Material.Builder(31000, gregtechId("primal_matter"))
-                .dust().fluid()
-                .fluidTemp(10)
+                .dust()
+                .liquid(new FluidBuilder().temperature(10))
                 .color(0xc86edc)
                 .iconSet(MaterialIconSet.NETHERSTAR).build();
 
         MRMaterials.ChargedMatter = new Material.Builder(31001, gregtechId("charged_matter"))
-                .fluid()
-                .fluidTemp(1)
+                .liquid(new FluidBuilder().temperature(1))
                 .color(0x0064c8)
                 .build();
 
         MRMaterials.NeutralMatter = new Material.Builder(31002, gregtechId("neutral_matter"))
-                .fluid()
-                .fluidTemp(1)
+                .liquid(new FluidBuilder().temperature(1))
                 .color(0xc80a14)
                 .build();
 
         MRMaterials.MatterAmplifier = new Material.Builder(31003, gregtechId("matter_amplifier"))
-                .dust().fluid()
-                .fluidTemp(300)
+                .dust()
+                .liquid(new FluidBuilder().temperature(300))
                 .color(0xda70d6)
                 .build();
     }
