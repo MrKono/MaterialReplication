135c2022061be062c33f66b8c6c2cf320ac69cd7
ko_no <90126004+MrKono@users.noreply.github.com>
2024-09-20 22:25:10 +0900

    fix: MTE Registries

--- src/main/java/kono/ceu/materialreplication/common/MREventHandlers.java
+++ src/main/java/kono/ceu/materialreplication/common/MREventHandlers.java
@@ -1,18 +1,23 @@
 package kono.ceu.materialreplication.common;
 
+import gregtech.api.GregTechAPI;
+import gregtech.api.metatileentity.registry.MTEManager;
+import gregtech.api.unification.material.event.MaterialEvent;
+import gregtech.api.unification.material.event.PostMaterialEvent;
+import kono.ceu.materialreplication.api.unification.materials.MRMaterials;
+import kono.ceu.materialreplication.api.unification.materials.flags.MRMaterialFlagAddition;
+import kono.ceu.materialreplication.api.util.MRValues;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import gregtech.api.unification.material.event.MaterialEvent;
-import gregtech.api.unification.material.event.PostMaterialEvent;
-
-import kono.ceu.materialreplication.api.unification.materials.MRMaterials;
-import kono.ceu.materialreplication.api.unification.materials.flags.MRMaterialFlagAddition;
-import kono.ceu.materialreplication.api.util.MRValues;
-
 @Mod.EventBusSubscriber(modid = MRValues.MODID)
 public class MREventHandlers {
+
+    @SubscribeEvent
+    public static void registerMTERegistry(MTEManager.MTERegistryEvent event) {
+        GregTechAPI.mteManager.createRegistry(MRValues.MODID);
+    }
 
     @SubscribeEvent(priority = EventPriority.HIGH)
     public static void registerMaterials(MaterialEvent event) {
--- src/main/java/kono/ceu/materialreplication/common/machines/MRMetaTileEntities.java
+++ src/main/java/kono/ceu/materialreplication/common/machines/MRMetaTileEntities.java
@@ -1,31 +1,28 @@
 package kono.ceu.materialreplication.common.machines;
 
-import static gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity;
-import static kono.ceu.materialreplication.api.util.MRValues.*;
-
-import net.minecraft.util.ResourceLocation;
-
-import org.jetbrains.annotations.NotNull;
-
 import gregtech.api.GTValues;
 import gregtech.api.GregTechAPI;
 import gregtech.api.metatileentity.SimpleMachineMetaTileEntity;
 import gregtech.api.util.GTUtility;
-
 import kono.ceu.materialreplication.api.recipes.MRRecipeMaps;
 import kono.ceu.materialreplication.client.MRTextures;
 import kono.ceu.materialreplication.common.machines.multi.MetaTileEntityLargeDeconstructor;
 import kono.ceu.materialreplication.common.machines.multi.MetaTileEntityLargeScrapper;
 import kono.ceu.materialreplication.common.machines.single.MetaTileEntityScrapMaker;
+import net.minecraft.util.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
+
+import static gregtech.common.metatileentities.MetaTileEntities.registerMetaTileEntity;
+import static kono.ceu.materialreplication.api.util.MRValues.*;
 
 public class MRMetaTileEntities {
 
-    // Single : 20000 - 200044
+    // Single : 0 -
     public static SimpleMachineMetaTileEntity[] DECONSTRUCTOR = new SimpleMachineMetaTileEntity[GTValues.V.length - 1];
     public static SimpleMachineMetaTileEntity[] REPLICATOR = new SimpleMachineMetaTileEntity[GTValues.V.length - 1];
     public static MetaTileEntityScrapMaker[] SCRAPPER = new MetaTileEntityScrapMaker[GTValues.V.length - 1];
 
-    // Multi : 21000
+    // Multi : 10000 -
     public static MetaTileEntityLargeDeconstructor LARGE_DECONSTRUCTOR;
     public static MetaTileEntityLargeScrapper LARGE_SCRAPPER;
 
@@ -35,53 +32,53 @@ public class MRMetaTileEntities {
     }
 
     public static void registerSingleMachine() {
-        // 20000 - 20014 : Material Deconstructor
+        // 0 - 14 : Material Deconstructor
         for (int i = 1; i < DECONSTRUCTOR.length; i++) {
             String voltageName = GTValues.VN[i].toLowerCase();
             if (!GregTechAPI.isHighTier() && i > GTValues.UV)
                 break;
             if (tierDeconstruct <= i)
-                DECONSTRUCTOR[i] = registerMetaTileEntity(20000 + (i - 1),
+                DECONSTRUCTOR[i] = registerMetaTileEntity((i - 1),
                         new SimpleMachineMetaTileEntity(
                                 new ResourceLocation(MODID, (String.format("%s.%s", "deconstructor", voltageName))),
                                 MRRecipeMaps.DECONSTRUCTION_RECIPES, MRTextures.DECONSTRUCTOR_OVERLAY, i, true,
                                 GTUtility.hvCappedTankSizeFunction));
         }
 
-        // 20015 - 20029 : Material Replicator
+        // 15 - 29 : Material Replicator
         for (int i = 1; i < REPLICATOR.length; i++) {
             String voltageName = GTValues.VN[i].toLowerCase();
             if (!GregTechAPI.isHighTier() && i > GTValues.UV)
                 break;
             if (tierReplicate <= i)
-                REPLICATOR[i] = registerMetaTileEntity(20015 + (i - 1),
+                REPLICATOR[i] = registerMetaTileEntity(15 + (i - 1),
                         new SimpleMachineMetaTileEntity(
                                 new ResourceLocation(MODID, (String.format("%s.%s", "replicator", voltageName))),
                                 MRRecipeMaps.REPLICATION_RECIPES, MRTextures.REPLICATOR_OVERLAY, i, true,
                                 GTUtility.hvCappedTankSizeFunction));
         }
 
-        // 20030 - 20044 Scrapper
-        for (int i = 0; i < SCRAPPER.length - 1; i++) {
-            String voltageName = GTValues.VN[i + 1].toLowerCase();
+        // 30 - 44 Scrapper
+        for (int i = 1; i < SCRAPPER.length - 1; i++) {
+            String voltageName = GTValues.VN[i].toLowerCase();
             if (!GregTechAPI.isHighTier() && i > GTValues.UV)
                 break;
-            SCRAPPER[i] = registerMetaTileEntity(20030 + i,
+            SCRAPPER[i] = registerMetaTileEntity(30 + (i - 1),
                     new MetaTileEntityScrapMaker(
                             new ResourceLocation(MODID, (String.format("%s.%s", "scrapmaker", voltageName))),
-                            MRRecipeMaps.SCRAPMAKER_RECIPES, MRTextures.SCRAPPER_OVERLAY, i + 1,
+                            MRRecipeMaps.SCRAPMAKER_RECIPES, MRTextures.SCRAPPER_OVERLAY, i,
                             GTUtility.hvCappedTankSizeFunction));
 
         }
     }
 
     public static void registerMultiMachine() {
-        // LargeDeconstructor 21000
-        LARGE_DECONSTRUCTOR = registerMetaTileEntity(21000,
+        // Large Deconstructor 10000
+        LARGE_DECONSTRUCTOR = registerMetaTileEntity(10000,
                 new MetaTileEntityLargeDeconstructor(mrId("large_deconstructor")));
 
-        // LargeScrapper 21001
-        LARGE_SCRAPPER = registerMetaTileEntity(21001,
+        // Large Scrapper 10001
+        LARGE_SCRAPPER = registerMetaTileEntity(10001,
                 new MetaTileEntityLargeScrapper(mrId("large_scrapper")));
     }
 
