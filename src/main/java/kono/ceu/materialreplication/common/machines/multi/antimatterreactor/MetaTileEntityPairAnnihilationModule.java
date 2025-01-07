2b1fa2db63c8aa2f1c40fba72387436cdcd45ffb
ko_no <90126004+MrKono@users.noreply.github.com>
2025-01-07 20:05:26 +0900

    add JEI pattern

--- src/main/java/kono/ceu/materialreplication/common/machines/multi/antimatterreactor/MetaTileEntityPairAnnihilationModule.java
+++ src/main/java/kono/ceu/materialreplication/common/machines/multi/antimatterreactor/MetaTileEntityPairAnnihilationModule.java
@@ -1,6 +1,13 @@
 package kono.ceu.materialreplication.common.machines.multi.antimatterreactor;
 
+import static gregtech.api.GTValues.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -13,16 +20,19 @@ import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.*;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.MultiblockShapeInfo;
 import gregtech.api.unification.material.Materials;
 import gregtech.client.renderer.ICubeRenderer;
 import gregtech.client.renderer.texture.Textures;
 import gregtech.common.blocks.BlockGlassCasing;
 import gregtech.common.blocks.MetaBlocks;
+import gregtech.common.metatileentities.MetaTileEntities;
 
 import kono.ceu.materialreplication.api.recipes.MRRecipeMaps;
 import kono.ceu.materialreplication.client.MRTextures;
 import kono.ceu.materialreplication.common.blocks.BlockAntimatterCasing;
 import kono.ceu.materialreplication.common.blocks.MRBlocks;
+import kono.ceu.materialreplication.common.machines.MRMetaTileEntities;
 
 public class MetaTileEntityPairAnnihilationModule extends FuelMultiblockController {
 
@@ -64,6 +74,55 @@ public class MetaTileEntityPairAnnihilationModule extends FuelMultiblockControll
                 .where('B', air().or(states(getCasingState())))
                 .where('#', any())
                 .build();
+    }
+
+    @Override
+    public List<MultiblockShapeInfo> getMatchingShapes() {
+        List<MultiblockShapeInfo> shapeInfos = new ArrayList<>();
+
+        MultiblockShapeInfo.Builder baseBuilder = MultiblockShapeInfo.builder()
+                .aisle("###############", "######CCC######", "####CC###CC####", "###C#######C###", "##C#########C##",
+                        "##C#########C##", "#C###########C#", "#C###########C#", "#C###########C#", "##C#########C##",
+                        "##C#########C##", "###C#######C###", "####CC###CC####", "######CCC######", "###############")
+                .aisle("######GGG######", "####GGAAAGG####", "###GAAGGGAAG###", "##GHGG#F#GGHG##", "#GAG###F###GAG#",
+                        "#GAG###F###GAG#", "GBG####G####GBG", "GAGFFFGRGFFFGAG", "GBG####G####GBG", "#GAG###F###GAG#",
+                        "#GAG###F###GAG#", "##IHEG#F#GeHi##", "###GAAGGGAAG###", "####GGAAAGG####", "######GSG######")
+                .aisle("###############", "######CCC######", "####CC###CC####", "###C#######C###", "##C#########C##",
+                        "##C#########C##", "#C###########C#", "#C###########C#", "#C###########C#", "##C#########C##",
+                        "##C#########C##", "###C#######C###", "####CC###CC####", "######CCC######", "###############")
+                .where('S', MRMetaTileEntities.MODULE_PAIR_ANNIHILATION, EnumFacing.SOUTH)
+                .where('G', getGlassState())
+                .where('C', getCasingState())
+                .where('H',
+                        MRBlocks.ANTIMATTER_CASING.getState(BlockAntimatterCasing.AntimatterCasingType.HELIUM_COOLANT))
+                .where('A', Blocks.AIR.getDefaultState())
+                .where('E', MetaTileEntities.ENERGY_OUTPUT_HATCH[UV], EnumFacing.EAST)
+                .where('e', MetaTileEntities.ENERGY_OUTPUT_HATCH[UV], EnumFacing.WEST)
+                .where('I', MetaTileEntities.FLUID_IMPORT_HATCH[LV], EnumFacing.WEST)
+                .where('i', MetaTileEntities.FLUID_IMPORT_HATCH[LV], EnumFacing.EAST)
+                .where('R', getCoreState())
+                .where('F', MetaBlocks.FRAMES.get(Materials.NaquadahAlloy).getBlock(Materials.NaquadahAlloy))
+                .where('B', getCasingState())
+                .where('#', Blocks.AIR.getDefaultState());
+
+        // Glass -> Casing
+        shapeInfos.add(baseBuilder.shallowCopy()
+                .where('G', getCasingState())
+                .build());
+
+        // Air -> Casing
+        shapeInfos.add(baseBuilder.shallowCopy()
+                .where('B', getCasingState())
+                .build());
+
+        // Glass -> Casing, Air -> Casing
+        shapeInfos.add(baseBuilder.shallowCopy()
+                .where('G', getCasingState())
+                .where('B', getCasingState())
+                .build());
+
+        shapeInfos.add(baseBuilder.build());
+        return shapeInfos;
     }
 
     private IBlockState getCasingState() {
--- src/main/java/kono/ceu/materialreplication/common/machines/multi/antimatterreactor/MetaTileEntityProcessorModule.java
+++ src/main/java/kono/ceu/materialreplication/common/machines/multi/antimatterreactor/MetaTileEntityProcessorModule.java
@@ -1,6 +1,13 @@
 package kono.ceu.materialreplication.common.machines.multi.antimatterreactor;
 
+import static gregtech.api.GTValues.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -10,20 +17,22 @@ import org.jetbrains.annotations.NotNull;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
 import gregtech.api.metatileentity.multiblock.IMultiblockPart;
-import gregtech.api.metatileentity.multiblock.MultiblockAbility;
 import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.pattern.MultiblockShapeInfo;
 import gregtech.api.unification.material.Materials;
 import gregtech.client.renderer.ICubeRenderer;
 import gregtech.client.renderer.texture.Textures;
 import gregtech.common.blocks.BlockGlassCasing;
 import gregtech.common.blocks.MetaBlocks;
+import gregtech.common.metatileentities.MetaTileEntities;
 
 import kono.ceu.materialreplication.api.recipes.MRRecipeMaps;
 import kono.ceu.materialreplication.client.MRTextures;
 import kono.ceu.materialreplication.common.blocks.BlockAntimatterCasing;
 import kono.ceu.materialreplication.common.blocks.MRBlocks;
+import kono.ceu.materialreplication.common.machines.MRMetaTileEntities;
 
 public class MetaTileEntityProcessorModule extends RecipeMapMultiblockController {
 
@@ -69,6 +78,63 @@ public class MetaTileEntityProcessorModule extends RecipeMapMultiblockController
                 .where('B', air().or(states(getCasingState())))
                 .where('#', any())
                 .build();
+    }
+
+    @Override
+    public List<MultiblockShapeInfo> getMatchingShapes() {
+        List<MultiblockShapeInfo> shapeInfos = new ArrayList<>();
+
+        MultiblockShapeInfo.Builder baseBuilder = MultiblockShapeInfo.builder()
+                .aisle("###############", "######GGG######", "###############")
+                .aisle("######CCC######", "####GGAAAGG####", "######CCC######")
+                .aisle("####CC###CC####", "###GAAGGGAAG###", "####CC###CC####")
+                .aisle("###C#######C###", "##GHOG#F#GMHG##", "###C#######C###")
+                .aisle("##C#########C##", "#GAG###F###GAG#", "##C#########C##")
+                .aisle("##C#########C##", "#GAG###F###GAG#", "##C#########C##")
+                .aisle("#C###########C#", "GBG####G####GBG", "#C###########C#")
+                .aisle("#C###########C#", "GAGFFFGRGFFFGAG", "#C###########C#")
+                .aisle("#C###########C#", "GBG####G####GBG", "#C###########C#")
+                .aisle("##C#########C##", "#GAG###F###GAG#", "##C#########C##")
+                .aisle("##C#########C##", "#GAG###F###GAG#", "##C#########C##")
+                .aisle("###C#######C###", "##IHEG#F#GeHi##", "###C#######C###")
+                .aisle("####CC###CC####", "###GAAGGGAAG###", "####CC###CC####")
+                .aisle("######CCC######", "####GGAAAGG####", "######CCC######")
+                .aisle("###############", "######GSG######", "###############")
+                .where('S', MRMetaTileEntities.MODULE_ANTIMATTER_PROCESSOR, EnumFacing.SOUTH)
+                .where('G', getGlassState())
+                .where('C', getCasingState())
+                .where('H',
+                        MRBlocks.ANTIMATTER_CASING.getState(BlockAntimatterCasing.AntimatterCasingType.HELIUM_COOLANT))
+                .where('A', Blocks.AIR.getDefaultState())
+                .where('E', MetaTileEntities.ENERGY_INPUT_HATCH[UV], EnumFacing.EAST)
+                .where('e', MetaTileEntities.ENERGY_INPUT_HATCH[UV], EnumFacing.WEST)
+                .where('I', MetaTileEntities.FLUID_IMPORT_HATCH[LV], EnumFacing.WEST)
+                .where('i', MetaTileEntities.FLUID_IMPORT_HATCH[LV], EnumFacing.EAST)
+                .where('M', MetaTileEntities.ITEM_IMPORT_BUS[LV], EnumFacing.SOUTH)
+                .where('O', MetaTileEntities.FLUID_EXPORT_HATCH[LV], EnumFacing.SOUTH)
+                .where('R', getCoreState())
+                .where('F', MetaBlocks.FRAMES.get(Materials.NaquadahAlloy).getBlock(Materials.NaquadahAlloy))
+                .where('B', getCasingState())
+                .where('#', Blocks.AIR.getDefaultState());
+
+        // Glass -> Casing
+        shapeInfos.add(baseBuilder.shallowCopy()
+                .where('G', getCasingState())
+                .build());
+
+        // Air -> Casing
+        shapeInfos.add(baseBuilder.shallowCopy()
+                .where('B', getCasingState())
+                .build());
+
+        // Glass -> Casing, Air -> Casing
+        shapeInfos.add(baseBuilder.shallowCopy()
+                .where('G', getCasingState())
+                .where('B', getCasingState())
+                .build());
+
+        shapeInfos.add(baseBuilder.build());
+        return shapeInfos;
     }
 
     private IBlockState getCasingState() {
